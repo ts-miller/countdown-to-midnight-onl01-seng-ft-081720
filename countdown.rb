ed8b103275570c7548908f672885648fc46edad8
Tyler Miller <tylerstephen11@gmail.com>
2020-08-18 18:39:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(number)
   
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+end
